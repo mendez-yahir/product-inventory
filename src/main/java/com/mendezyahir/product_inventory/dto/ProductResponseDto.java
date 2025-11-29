b4c4d969307d41c198c00235736ac9bdec941f75
Mendez <yahirelian.mendez@outlook.com>
2025-11-29 16:57:40 +0100

    feat: add ProductResponseDto for API responses

--- src/main/java/com/mendezyahir/product_inventory/dto/ProductResponseDto.java
+++ src/main/java/com/mendezyahir/product_inventory/dto/ProductResponseDto.java
@@ -1,4 +1,14 @@
 package com.mendezyahir.product_inventory.dto;
 
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Getter
 public class ProductResponseDto {
+    private Long id;
+    private String name;
+    private String description;
+    private ProductBrandResponseDto productBrand;
+    private Set<ProductCategoryResponseDto> productCategories;
 }
