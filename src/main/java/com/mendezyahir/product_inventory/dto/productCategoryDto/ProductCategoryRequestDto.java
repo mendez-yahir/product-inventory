a0640cec2d6434846b0eace9e2c6035425bb1969
Mendez <yahirelian.mendez@outlook.com>
2025-11-29 19:34:06 +0100

    feat: add ProductCategoryRequestDto and validations

---
+++ src/main/java/com/mendezyahir/product_inventory/dto/productCategoryDto/ProductCategoryRequestDto.java
@@ -0,0 +1,19 @@
+package com.mendezyahir.product_inventory.dto.productCategoryDto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
+
+public class ProductCategoryRequestDto {
+    @NotBlank(message = "The product category name is required.")
+    @Size(min = 3, max = 100, message = "The product category name must be between 3 and 100 characters.")
+    private String name;
+
+    @NotBlank(message = "The product category description is required.")
+    @Size(min = 20, max = 512, message = "The product category description must be between 20 and 512 characters.")
+    private String description;
+
+    @Size(min = 10, max = 2000, message = "The size URL must be between 10 and 2000 characters.")
+    @URL(message = "The size guide URL must be a valid URL.")
+    private String sizeGuideUrl;
+}
